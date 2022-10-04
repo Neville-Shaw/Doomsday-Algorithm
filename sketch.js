3902b3751de17297693bbad3e043beadb3bdf2a1
Neville Shaw <84479581+Neville-Shaw@users.noreply.github.com>
2022-10-04 15:05:08 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,9 +15,8 @@ function setup() {
   // initilize the canvas position to the canvas container div
   let canvas = createCanvas(700, 700);
   canvas.parent("canvascontainer");
- 
-  CreateLookupTable(lookupTable);
 
+  CreateLookupTable(lookupTable);
 }
 
 function draw() {
@@ -26,18 +25,43 @@ function draw() {
 }
 
 function CreateDoomsday() {
-
-  
-
+  // Separate the date input into multiple outputs
   let month = GetData(Qdate, "month");
   let day = GetData(Qdate, "day");
   let year = GetData(Qdate, "year");
-
   console.log("Date:", lookupTable[0][month - 1] + ",", day + ",", year);
 
-  // Find Doomsday.
-  year %= 100;
-  // Check leap year
+  // ------------ Get anchor day for the century ------------ //
+  let century = floor(year / 100) % 4;
+  if (century == 0) {
+    anchorCentury = 2;
+  } else if (century == 1) {
+    anchorCentury = 0;
+  } else if (century == 2) {
+    anchorCentury = 5;
+  } else if (century == 3) {
+    anchorCentury = 3;
+  }
+  console.log("anchorcentury", anchorCentury);
+  // ------------------------------------------------------ //
+
+  // --------------- Get the anchor day for the last two digits of year ---------- //
+  let L2 = year % 100;
+  // Inc every 12 months
+  let a = floor(L2 / 12);
+  // get remainder
+  let b = last2 % 12;
+  // account for leap years using b
+  let c = floor(b / 4);
+
+  // Calc total
+  let total = a + b + c;
+  // ----------------------------------------------------------- //
+
+  // total both up and % 7 to get num within days of the week
+  let doomsday = (total + anchorCentury) % 7;
+
+  // Check if its a leap year because that alters the anchor day for some months
   let leapYear;
   if (year % 4 != 0) {
     leapYear = false;
@@ -49,34 +73,10 @@ function CreateDoomsday() {
     leapYear = true;
   }
 
-  // Get anchor day for the year
-  let century = floor(year / 100) % 4;
-  if (century == 0) {
-    anchorCentury = 2;
-  } else if (century == 1) {
-    anchorCentury = 0;
-  } else if (century == 2) {
-    anchorCentury = 5;
-  } else if (century == 3) {
-    anchorCentury = 3;
-  }
-
-  // Use the "Odd + 11" method.
-  let anchorYear = year;
-  if (anchorYear % 2 == 1) {
-    anchorYear += 11;
-  }
-  anchorYear = anchorYear / 2;
-  if (anchorYear % 2 == 1) {
-    anchorYear += 11;
-  }
-  anchorYear = 7 - (anchorYear % 7);
-  let doomsday = (anchorYear + anchorCentury) % 7;
-  console.log("Anchor Day:", lookupTable[1][doomsday]);
-
-  // Find the anchor day for the month.
+  // Find the anchor day for the chosen month.
   let monthAnchor = gatherDoom(month, leapYear);
 
+  // calculate how far away the nearest anchor day is from day input and change day accordingly //
   if (abs(day - monthAnchor) % 7 != 0 && day != monthAnchor) {
     let temp = abs(monthAnchor - day);
     temp = temp % 7;
@@ -95,10 +95,12 @@ function CreateDoomsday() {
     }
   }
 
-  console.log("Doomsday:", lookupTable[1][doomsday]);
-
+  // Convert numerical months to alphabetical and display
   FinalDate.html(
     lookupTable[0][month - 1] + ", " + day + ", " + GetData(Qdate, "year")
   );
+
+  // Convert numerical day of the week to alphabetical and display
   FinalDay.html("--" + lookupTable[1][doomsday] + "--");
+  console.log("Doomsday:", lookupTable[1][doomsday]);
 }
